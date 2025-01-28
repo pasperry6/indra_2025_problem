4a150c6de4ce5cf2ac8ff60e85b01bf9653a2963
Peter Sperry <psperry78@gmail.com>
2025-01-28 01:31:50 -0500

    output is unordered map of 1,2,3,4 grams, but is correct

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # indra_2025_problem
-questions:
+# questions:
 1. what is the input file type?
 2. what is the output file type and format?
 3. 
 
-to check:
+# to check:
 1. works with cpp 11.
 2. worst case analysis.
 
 
-pre-work (thought) process:
+# pre-work (thought) process:
 I first thought of the most simple (incorrect) ways that this could be done. I thought that it would be to keep a list of grams 
 and go through the list every time i found a new gram. That would be terrible. Worst case analysis:
 In the worst case, the program would fill a list with letters and would have to go down that list every time to find the correct gram. 
@@ -24,8 +24,12 @@ would be 26^2. Therefore, I have to use a variably-sized hashmap with a sorting 
 I have to think about the way to iterate through each letter. My first thought is that each lesser gram is included in the first 
 1,2,3 characters of the 4 gram, so I just go through the 4 grams from the start. 
 
-work process:
+# work process:
 1/27 
 I always start with the most simple case and I move from there. I was able to get a list of all the 
 four grams in the file at the end of the day.
 
+1/28
+I realized that it is so much easier to use an inbuilt hashmap, so I will not need to worry about the
+internals of the hash function and sizing. If I did do it in C, I would need to make hashmap on my own.
+I would make variably sized maps that double in size when about to overflow. I would also use a binary search to look for and insert the grams.
--- sol.cpp
+++ sol.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <unordered_map>
 
 using namespace std;
 
 class solver {
 public:
     string filename;
+    unordered_map<string, int> map;
 
     void main_solver(void) {
         ifstream file(filename);
         char ch;
         string fourg;
 
+        // iterate through file
         while (file.get(ch)) {
             fourg = fourg + ch; //add new char to create four gram
 
@@ -21,22 +24,48 @@ public:
                 continue;
             }
 
+            mapping(fourg); // going through each one, two, three, four grams in the four gram
+
             // to handle end of files
-            char one_ahead = file.peek();
-            if (one_ahead == EOF) {
+            if (file.peek() == EOF) {
                 // handle the threeg, twog, and oneg
-                cout<<fourg<<endl;
-                fourg.erase(0, 1);
-                cout<<fourg<<endl;
-                fourg.erase(0, 1);
-                cout<<fourg<<endl;
-                fourg.erase(0, 1);
-                cout<<fourg<<endl;
+                fourg.erase(0,1);
+                mapping(fourg);
+                fourg.erase(0,1);
+                mapping(fourg);
+                fourg.erase(0,1);
+                mapping(fourg);
                 break; //done with file
             }
 
-            cout << fourg << endl;
-            fourg.erase(0, 1);
+            fourg.erase(0, 1); //delete first char (to make room for new char)
+        }
+
+        // write to output file
+        write_output();
+    }
+
+    void write_output(void) {
+        ofstream file("output.txt");
+
+        // Write map content to the file
+        for (std::unordered_map<std::string, int>::iterator it = map.begin(); it != map.end(); ++it) {
+            file << it->first << " : " << it->second << std::endl;
+        }
+
+        file.close();
+    }
+
+    void mapping(string gram) {
+        int size = gram.length();
+        for (int i = 0; i < size; i++) {
+            if (map.find(gram) == map.end()) {
+                map[gram] = 1;
+            }
+            else {
+                map[gram] = map[gram] + 1;
+            }
+            gram.pop_back();
         }
     }
 
